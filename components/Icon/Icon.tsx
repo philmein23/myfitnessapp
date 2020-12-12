a3fa501fa6013e7e1b6806e7989c8c31d0be3a47
philmein23 <phillip.d.nguyen23@gmail.com>
2020-12-11 20:57:39 -0800

    feat: created Icon component for reusuable svgs

---
+++ components/Icon/Icon.tsx
@@ -0,0 +1,21 @@
+const icons = {
+  checkmark:
+    "M198.717 458.848l-198.934-195.8 98.689-97.135 100.245 98.666 214.81-211.426 98.689 97.135-313.499 308.56zM53.956 263.048l144.762 142.481 259.328-255.241-44.518-43.816-214.81 211.426-100.245-98.667-44.517 43.817z",
+};
+
+interface IconProps {
+  icon: string;
+  width: string;
+  height: string;
+  fill: string;
+}
+
+const Icon: React.FC<IconProps> = ({ icon, width, height, fill }) => {
+  return (
+    <svg fill={fill} width={width} height={height} viewBox="0 0 1024 1024">
+      <path d={icons[icon]}></path>
+    </svg>
+  );
+};
+
+export default Icon;
--- pages/new-program/index.tsx
+++ pages/new-program/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import useLocalStorage from "@hooks/useLocalStorage/useLocalStorage";
+import Icon from "@components/Icon/Icon";
 
 import styles from "./new-program.module.css";
 
